e258be0064384f0f1ced836b5f7256cfacc99c55
SAM BHU <SAM BHU@DESKTOP-5MMOUET>
2022-07-21 21:48:03 +0545

    Add project files.

---
+++ UserDefineException/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UserDefineException
+{
+
+    class MyException : Exception
+    {
+        public MyException(String str)
+        {
+            Console.WriteLine("User Define Exception");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("SHAMBHU");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught here" + e.ToString());
+            }
+            Console.WriteLine("LAST STATEMENT");
+        }
+       
+    }
+}
